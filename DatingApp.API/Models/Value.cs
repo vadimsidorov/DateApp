39b1dd92de452db3e6408c34a885ddc081a2b979
Vadim Sidorov <ecdysteron@gmail.com>
2019-10-10 01:19:36 -0400

    Initial commit

---
+++ DatingApp.API/Models/Value.cs
@@ -0,0 +1,11 @@
+namespace DatingApp.API.Models
+{
+    public class Value
+    {
+        public int MyProperty { get; set; }
+
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+    }
+}
